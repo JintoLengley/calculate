64c58ca1c4fe0b26ea229cddbeb0350276feccc3
JintoLengley <jintolengley@gmail.com>
2024-11-06 21:23:40 +0300

    calc

---
+++ калькулятор/калькулятор/Form1.Designer.cs
@@ -0,0 +1,379 @@
+﻿namespace калькулятор
+{
+    partial class Калькулятор
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            button1 = new Button();
+            label1 = new Label();
+            n0 = new Button();
+            n1 = new Button();
+            n2 = new Button();
+            n3 = new Button();
+            n4 = new Button();
+            n5 = new Button();
+            n6 = new Button();
+            n7 = new Button();
+            n8 = new Button();
+            n9 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Calibri", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBox1.Location = new Point(12, 12);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(320, 45);
+            textBox1.TabIndex = 0;
+            textBox1.TextAlign = HorizontalAlignment.Right;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 91);
+            button1.Name = "button1";
+            button1.Size = new Size(102, 43);
+            button1.TabIndex = 2;
+            button1.Text = "=";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(63, 148);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 15);
+            label1.TabIndex = 3;
+            // 
+            // n0
+            // 
+            n0.BackColor = SystemColors.ButtonShadow;
+            n0.Font = new Font("Arial", 14.25F);
+            n0.Location = new Point(64, 197);
+            n0.Name = "n0";
+            n0.Size = new Size(50, 49);
+            n0.TabIndex = 6;
+            n0.Text = "0";
+            n0.UseVisualStyleBackColor = false;
+            n0.Click += n0_Click;
+            // 
+            // n1
+            // 
+            n1.BackColor = SystemColors.ButtonShadow;
+            n1.Font = new Font("Arial", 14.25F);
+            n1.Location = new Point(146, 197);
+            n1.Name = "n1";
+            n1.Size = new Size(50, 49);
+            n1.TabIndex = 7;
+            n1.Text = "1";
+            n1.UseVisualStyleBackColor = false;
+            n1.Click += n1_Click;
+            // 
+            // n2
+            // 
+            n2.BackColor = SystemColors.ButtonShadow;
+            n2.Font = new Font("Arial", 14.25F);
+            n2.Location = new Point(231, 197);
+            n2.Name = "n2";
+            n2.Size = new Size(50, 49);
+            n2.TabIndex = 8;
+            n2.Text = "2";
+            n2.UseVisualStyleBackColor = false;
+            n2.Click += button4_Click;
+            // 
+            // n3
+            // 
+            n3.BackColor = SystemColors.ButtonShadow;
+            n3.Font = new Font("Arial", 14.25F);
+            n3.Location = new Point(64, 278);
+            n3.Name = "n3";
+            n3.Size = new Size(50, 49);
+            n3.TabIndex = 9;
+            n3.Text = "3";
+            n3.UseVisualStyleBackColor = false;
+            n3.Click += n3_Click;
+            // 
+            // n4
+            // 
+            n4.BackColor = SystemColors.ButtonShadow;
+            n4.Font = new Font("Arial", 14.25F);
+            n4.Location = new Point(146, 278);
+            n4.Name = "n4";
+            n4.Size = new Size(50, 49);
+            n4.TabIndex = 10;
+            n4.Text = "4";
+            n4.UseVisualStyleBackColor = false;
+            n4.Click += n4_Click;
+            // 
+            // n5
+            // 
+            n5.BackColor = SystemColors.ButtonShadow;
+            n5.Font = new Font("Arial", 14.25F);
+            n5.Location = new Point(231, 278);
+            n5.Name = "n5";
+            n5.Size = new Size(50, 49);
+            n5.TabIndex = 11;
+            n5.Text = "5";
+            n5.UseVisualStyleBackColor = false;
+            n5.Click += n5_Click;
+            // 
+            // n6
+            // 
+            n6.BackColor = SystemColors.ButtonShadow;
+            n6.Font = new Font("Arial", 14.25F);
+            n6.Location = new Point(63, 358);
+            n6.Name = "n6";
+            n6.Size = new Size(50, 49);
+            n6.TabIndex = 12;
+            n6.Text = "6";
+            n6.UseVisualStyleBackColor = false;
+            n6.Click += n6_Click;
+            // 
+            // n7
+            // 
+            n7.BackColor = SystemColors.ButtonShadow;
+            n7.Font = new Font("Arial", 14.25F);
+            n7.Location = new Point(146, 358);
+            n7.Name = "n7";
+            n7.Size = new Size(50, 49);
+            n7.TabIndex = 13;
+            n7.Text = "7";
+            n7.UseVisualStyleBackColor = false;
+            n7.Click += button9_Click;
+            // 
+            // n8
+            // 
+            n8.BackColor = SystemColors.ButtonShadow;
+            n8.Font = new Font("Arial", 14.25F);
+            n8.Location = new Point(231, 358);
+            n8.Name = "n8";
+            n8.Size = new Size(50, 49);
+            n8.TabIndex = 14;
+            n8.Text = "8";
+            n8.UseVisualStyleBackColor = false;
+            n8.Click += n8_Click;
+            // 
+            // n9
+            // 
+            n9.BackColor = SystemColors.ButtonShadow;
+            n9.Font = new Font("Arial", 14.25F);
+            n9.Location = new Point(146, 438);
+            n9.Name = "n9";
+            n9.Size = new Size(50, 49);
+            n9.TabIndex = 15;
+            n9.Text = "9";
+            n9.UseVisualStyleBackColor = false;
+            n9.Click += n9_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Arial", 14.25F);
+            button2.Location = new Point(118, 91);
+            button2.Name = "button2";
+            button2.Size = new Size(50, 43);
+            button2.TabIndex = 16;
+            button2.Text = "+";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Arial", 14.25F);
+            button3.Location = new Point(118, 140);
+            button3.Name = "button3";
+            button3.Size = new Size(50, 43);
+            button3.TabIndex = 17;
+            button3.Text = "-";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Arial", 14.25F);
+            button4.Location = new Point(174, 91);
+            button4.Name = "button4";
+            button4.Size = new Size(50, 43);
+            button4.TabIndex = 18;
+            button4.Text = "/";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click_1;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Arial", 14.25F);
+            button5.Location = new Point(174, 140);
+            button5.Name = "button5";
+            button5.Size = new Size(50, 43);
+            button5.TabIndex = 19;
+            button5.Text = "*";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Arial", 14.25F);
+            button6.Location = new Point(230, 91);
+            button6.Name = "button6";
+            button6.Size = new Size(50, 43);
+            button6.TabIndex = 20;
+            button6.Text = "C";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("Arial", 14.25F);
+            button7.Location = new Point(230, 140);
+            button7.Name = "button7";
+            button7.Size = new Size(50, 43);
+            button7.TabIndex = 21;
+            button7.Text = "-/+";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.BackColor = SystemColors.ButtonShadow;
+            button8.Font = new Font("Arial", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button8.Location = new Point(231, 438);
+            button8.Name = "button8";
+            button8.Size = new Size(50, 49);
+            button8.TabIndex = 22;
+            button8.Text = ".";
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.Font = new Font("Arial", 14.25F);
+            button9.Location = new Point(286, 91);
+            button9.Name = "button9";
+            button9.Size = new Size(56, 43);
+            button9.TabIndex = 23;
+            button9.Text = "M1";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click_1;
+            // 
+            // button10
+            // 
+            button10.Font = new Font("Arial", 14.25F);
+            button10.Location = new Point(286, 140);
+            button10.Name = "button10";
+            button10.Size = new Size(56, 43);
+            button10.TabIndex = 24;
+            button10.Text = "M1+";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // button11
+            // 
+            button11.BackColor = SystemColors.GradientActiveCaption;
+            button11.Font = new Font("Arial", 14.25F);
+            button11.Location = new Point(12, 443);
+            button11.Name = "button11";
+            button11.Size = new Size(74, 49);
+            button11.TabIndex = 25;
+            button11.Text = "фон";
+            button11.UseVisualStyleBackColor = false;
+            button11.Click += button11_Click;
+            // 
+            // Калькулятор
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDark;
+            ClientSize = new Size(346, 501);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(n9);
+            Controls.Add(n8);
+            Controls.Add(n7);
+            Controls.Add(n6);
+            Controls.Add(n5);
+            Controls.Add(n4);
+            Controls.Add(n3);
+            Controls.Add(n2);
+            Controls.Add(n1);
+            Controls.Add(n0);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Name = "Калькулятор";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private Label label1;
+        private Button n0;
+        private Button n1;
+        private Button n2;
+        private Button n3;
+        private Button n4;
+        private Button n5;
+        private Button n6;
+        private Button n7;
+        private Button n8;
+        private Button n9;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+    }
+}
---
+++ калькулятор/калькулятор/Form1.cs
@@ -0,0 +1,273 @@
+using System.Security;
+
+namespace калькулятор
+{
+    public partial class Калькулятор : Form
+    {
+
+        public Калькулятор()
+        {
+            InitializeComponent();
+        }
+        float a, b, c;
+        int count;
+        bool z = true;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            calculate();
+            label1.Text = "";
+        }
+        private void calculate()
+        {
+            switch (count)
+            {
+                case 1:
+                    b = a + float.Parse(textBox1.Text);
+                    textBox1.Text = b.ToString();
+                    break;
+                case 2:
+                    b = a - float.Parse(textBox1.Text);
+                    textBox1.Text = b.ToString();
+                    break;
+                case 3:
+                    b = a * float.Parse(textBox1.Text);
+                    textBox1.Text = b.ToString();
+                    break;
+                case 4:
+                    b = a / float.Parse(textBox1.Text);
+                    textBox1.Text = b.ToString();
+                    break;
+
+                default:
+                    break;
+            }
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "2";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "2";
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "7";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "7";
+            }
+        }
+
+        private void n0_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "0";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "0";
+            }
+        }
+
+        private void n1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "1";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "1";
+            }
+        }
+
+        private void n3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "3";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "3";
+            }
+        }
+
+        private void n4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "4";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "4";
+            }
+        }
+
+        private void n5_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "5";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "5";
+            }
+        }
+
+        private void n6_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "6";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "6";
+            }
+        }
+
+        private void n8_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "8";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "8";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void n9_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" && textBox1.Text != null)
+            {
+                textBox1.Text = "9";
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + "9";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 1;
+            label1.Text = a.ToString() + "+";
+            z = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 2;
+            label1.Text = a.ToString() + "-";
+            z = true;
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 4;
+            label1.Text = a.ToString() + "/";
+            z = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 3;
+            label1.Text = a.ToString() + "*";
+            z = true;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            label1.Text = "";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (z == true)
+            {
+                textBox1.Text = "-" + textBox1.Text;
+                z = false;
+            }
+            else if (z == false)
+            {
+                textBox1.Text = textBox1.Text.Replace("-", "");
+                z = true;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+
+        {
+            textBox1.Text = textBox1.Text + ",";
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click_1(object sender, EventArgs e)
+        {
+            c = float.Parse(textBox1.Text);
+            label1.Text = c.ToString() + "M1";
+            z = true;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = c.ToString();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            BackColor = new Color();
+            BackColor = Color.Aqua;
+        }
+
+        public static implicit operator Калькулятор(Color v)
+        {
+            throw new NotImplementedException();
+            
+        }
+
+
+    }
+}
+         
+    
+    
+
+
+
+
